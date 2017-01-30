b4aba4120cc4fe1385e13c9ff5a38a85d5436b4b
billy <billychrist2005@yahoo.fr>
2017-01-30 10:25:12 +0100

    même si j'ai créé toute la page de connnexion, je ne t'ai mis ici que l'élément essentiel à savoir, le formulaire de connexion

---
+++ connexion_extranet.php
@@ -0,0 +1,16 @@
+
+	<!--mot aux conseillers-->	
+	<br/> <div class="cdm1">Espace réservé aux conseillers-ères de la Cité des métiers.</div><br/>
+
+	
+	<!--formulaire-->	
+	<div class="cdm2">	
+		<form action="http://xxxxxxxxx/xxxxxxxxxx/traitement_formulaire.php" method="POST" class="form-signin" > <br/>		
+		
+			<input type="TEXT" name="connecta" id="inputTEXT" class="form-control" placeholder="Identifiant" required autofocus> <br/>
+			<input type="password" name="connectb" id="inputPASSWORD" class="form-control" placeholder="Mot de passe" required> <BR/>	
+					
+			<button class="btn btn-primary btn-block" type="submit" name="buttonconnect">Se connecter</button> 
+		</form>		
+	</div>		
+	
