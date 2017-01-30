b6db216e218139cf2e5b8553c7246df7ffe071cb
billy <billychrist2005@yahoo.fr>
2017-01-30 10:34:38 +0100

    et voici le fichier de traitement ki est appelé par le formulaire

---
+++ traitement_formulaire.php
@@ -0,0 +1,21 @@
+﻿<?php					//traitement des identifiants
+																													
+	$bdd=new PDO ('mysql:host=xxxxxxxx;dbname=xxxxxxxx','xxxxxxxx','xxxxxxxx');  
+	
+	if (isset($_POST ['buttonconnect']))
+			{	
+				session_start ();											
+				$_SESSION ['connecta'] = $_POST ['connecta'];				
+								
+				$identifiant= trim (htmlspecialchars($_POST ['connecta']));
+				$mdp= trim (htmlspecialchars($_POST ['connectb']));
+				
+				$requser= $bdd->prepare ("SELECT * FROM extranet_users WHERE extranet_login=? AND user_nicename=?");
+				$requser->execute(array($identifiant,$mdp));
+				$userexist= $requser-> rowCount();
+
+				if ($userexist >= 1) {header('Location: http://www.citedesmetiers-valdemarne.fr/espace-conseillers/');}     						
+				else {header('Location: http://www.citedesmetiers-valdemarne.fr/accueil-refuse-extranet-conseillers/');}								
+			}
+?>
+
